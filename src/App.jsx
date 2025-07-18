1d26d3f2cdf473540e94e4078a1c8eab08b40dca
Omnia Mohamed <omniamuhamed9191@gmail.com>
2025-07-19 00:02:45 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,10 @@ import TreePage from "./Components/treePage/TreePage";
 import FamilyZoom from "./Components/FamilyZoom";
 
 function App() {
-<<<<<<< HEAD
   return <FamilyZoom family={family} />;
-=======
   
-  return (
-    <div>
-      {/* <PasswordGate family={correctPassword}/>
-      <StatsCards family={family} />  */}
-      <FamilyTree family={family} />  
-    </div>
-  );
->>>>>>> e78cc9bf4cf4ef187338f994c11b0219285e2324
+
 }
 
 export default App;
+
